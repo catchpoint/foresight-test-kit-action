a18b7ea485b47cb636e12b65ce423399a2136849
gokhan721 <33860737+gokhan721@users.noreply.github.com>
2022-06-13 01:47:02 +0300

    add project_id

--- src/actions/helper/run_cli.ts
+++ src/actions/helper/run_cli.ts
@@ -10,7 +10,7 @@ export async function runCommand(command: string, args: string[] = [], envVariab
 }
 
 export async function generateCommand(apiKey: string, frameworkType: string, framework: string, paths: string[]) {
-    let command = `thundra-foresight-cli upload-${frameworkType} -a ${apiKey} -f ${framework}`;
+    let command = `thundra-foresight-cli upload-${frameworkType} -a ${apiKey} -p bc28ab0f-37eb-41b9-b360-182acf3546f2 -f ${framework}`;
     for(const path of paths) {
         command += ` -ud ${path}`
     }
