c711798507094575efc77805165ff63c17a86559
erdogan.yesil <erdogan@thundra.io>
2022-09-26 15:29:57 +0300

    on demand api key support

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,12 @@ export interface JobInfo {
     readonly id?: number
     readonly name?: string
 }
+
+export interface ApiKeyInfo {
+    readonly apiKey?: string | undefined
+}
+
+export interface OnDemandAPIKeyParam {
+    readonly repoFullName: string | undefined
+    readonly workflowRunId: number | undefined
+}
